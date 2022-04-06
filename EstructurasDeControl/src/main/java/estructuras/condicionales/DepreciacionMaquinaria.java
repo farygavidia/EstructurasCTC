3d626bcc6f0286cb942bbdf5cbcb81158d61dbb8
Manuel Alvarado <manuel.alvarado21@itca.edu.sv>
2022-04-05 22:18:42 -0600

    update
    ejercicio in process

--- EstructurasDeControl/src/main/java/estructuras/condicionales/DepreciacionMaquinaria.java
+++ EstructurasDeControl/src/main/java/estructuras/condicionales/DepreciacionMaquinaria.java
@@ -12,15 +12,25 @@ public class DepreciacionMaquinaria {
     public static void main(String[] args) {
         try{
             BufferedReader leer = new BufferedReader (new InputStreamReader(System.in));
-            System.out.println("Ingrese el nombre de la maquinaria: ");
-            String nombre = leer.readLine();
+            System.out.println("Ingresa la informacion solicitada a continuacion: ");
             System.out.println("Ingrese el costo de la maquinaria:");
             double costo = Double.parseDouble(leer.readLine());
             System.out.println("Ingrese el periodo de tiempo en anhios de depreciacion");
             int anhios = Integer.parseInt(leer.readLine());
-            calculoDepreciacion(vidaUtil, sumaDigitos, valorActivo);
+            System.out.println("la cantidad: ");
+            double cantidad = Double.parseDouble(leer.readLine());
+            
+            calculoDepreciacion(cantidad, costo, anhios);
         }catch(Exception e){
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static void calculoDepreciacion(double costo, double cantidad, int anhios) {
+        for(int i= 1 ; i<= anhios; i++){
+            double sumaDigitos = cantidad * anhios;
+            cantidad += (anhios/sumaDigitos)*costo;
+            System.out.println("La depreciacion Es de :" + cantidad);
         }
     }
 }
